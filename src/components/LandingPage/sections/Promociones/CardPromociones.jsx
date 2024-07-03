b13eb8f92f690c882112c34b52688182879bee78
JDR89 <joaquin.romero89@gmail.com>
2024-07-03 16:33:15 -0300

    subiendo seccion promociones

--- src/components/LandingPage/sections/Promociones/CardPromociones.jsx
+++ src/components/LandingPage/sections/Promociones/CardPromociones.jsx
@@ -1,31 +1,28 @@
-import { Card, CardFooter, Image, Button } from "@nextui-org/react";
-import IMG_Prompciones_01 from "../../../../assets/Img/Promociones_01.webp";
+import { Card, CardFooter } from "@nextui-org/react";
+import IMG_Promociones_05 from "../../../../assets/Img/Promociones_05.webp";
 
 export const CardPromociones = () => {
   return (
     <Card
       radius="lg"
-      className="border-none shadow-xl shadow-gray-500 h-200 max-h-[200px] overflow-hidden"
+      className=" h-[350px] md:h-[361px] shadow-sm shadow-gray-500 overflow-hidden"
     >
-      <Image
-        alt="Promocion 05"
-        height={200}
-        src={IMG_Prompciones_01}
-        width={1000}
-        className="origin-bottom w-full"
-      />
-      <CardFooter className="justify-between bg-black/55 border-black/70 border-1 overflow-hidden absolute rounded-large bottom-0 w-full shadow-small z-10">
+      <div className="relative w-full h-full">
+        <img
+          alt="Promocion 05"
+          src={IMG_Promociones_05}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <CardFooter className="justify-between bg-black/55 border-black/70 border-1 overflow-hidden absolute bottom-0 w-full shadow-small z-10">
         <div>
-          <h4 className="text-freshMint text-tiny">Lorem</h4>
-          <p className="text-lg font-bold text-white">$4500</p>
+          <h4 className="text-freshMint text-md font-nunito">Lorem ipsum</h4>
+          <div className="flex items-center ">
+            <p className="text-lg font-bold text-white">$4500</p>
+            <p className="text-freshMint text-md mx-2 font-nunito">Precio por persona</p>
+          </div>
         </div>
-        <Button
-          className="bg-greenT font-bold text-tiny text-white"
-          radius="lg"
-          size="sm"
-        >
-          Ver sitio
-        </Button>
+       
       </CardFooter>
     </Card>
   );
--- src/components/LandingPage/sections/Promociones/Promociones.jsx
+++ src/components/LandingPage/sections/Promociones/Promociones.jsx
@@ -1,34 +1,31 @@
-
-
-
+import { CardPromociones } from "./CardPromociones";
 
 
 export const Promociones = () => {
   return (
-    <main className="bg-gray100 w-full px-2 xl:px-32 lg:px-24 md:px-16 sm:px-8 xs:px-6 2xs:px-3">
+    <main className="bg-gray100 w-full px-2 xl:px-32 lg:px-24 md:px-16 sm:px-8 xs:px-6 2xs:px-3 h-auto">
       <section className="py-8 ">
         <h4 className="text-freshMint text-md">MEJORES OFERTAS</h4>
         <h1 className="text-2xl font-bold">Promociones imperdibles</h1>
 
         <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-4 my-8">
-      <div className="border border-blue-500 lg:col-span-2 h-48 flex items-center justify-center">
-      
-      <p className="text-center">Card 1</p>
-      </div>
-      <div className="border border-blue-500 h-48 flex items-center justify-center">
-        <p className="text-center">Card 2</p>
-      </div>
-      <div className="border border-blue-500 h-48 flex items-center justify-center">
-        <p className="text-center">Card 3</p>
-      </div>
-      <div className="border border-blue-500 h-48 flex items-center justify-center">
-        <p className="text-center">Card 4</p>
-      </div>
-      <div className="border border-blue-500 h-48 flex items-center justify-center">
-        <p className="text-center">Card 5</p>
-      </div>
-    </div>
-        
+          
+          <div className="border lg:col-span-2 h-[350px] md:h-[361px] ">
+          <CardPromociones />
+          </div>
+          <div className="border h-[350px] md:h-[361px] ">
+            <CardPromociones />
+          </div>
+          <div className="border h-[350px] md:h-[361px] ">
+          <CardPromociones />
+          </div>
+          <div className="border h-[350px] md:h-[361px] ">
+          <CardPromociones />
+          </div>
+          <div className="border h-[350px] md:h-[361px] ">
+          <CardPromociones />
+          </div>
+        </div>
       </section>
     </main>
   );
