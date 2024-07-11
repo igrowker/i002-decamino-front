6ec83958f131a5850d427f9f6b550d48cb9ff23c
wulf <czh1983@gmail.com>
2024-07-11 02:27:02 -0300

    Componente Routes integrado con Leaflet , permisos para pedir ubicacion real y boceto con inputs para escribir datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,190 @@
-import { Chip } from "@nextui-org/react"
-import { Navigate, Route, Routes } from "react-router-dom"
-import { Landing_Page } from "./Pages/LandingPage/Landing_Page.View"
+import { Chip } from "@nextui-org/react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Landing_Page } from "./Pages/LandingPage/Landing_Page.View";
+
+import { RoutePlanner } from "./components/RoutePlanner";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path="Home" element={
-        <Landing_Page/>
-      }>
-      </Route>
+      <Routes>
+        <Route path="Home" element={<Landing_Page />}></Route>
 
-      <Route path="Profile" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="danger" variant="shadow">PROFILE</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="Profile"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-white font-bold"
+                color="danger"
+                variant="shadow"
+              >
+                PROFILE
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path="Payment" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="success" variant="shadow">PAYMENT</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="Payment"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-white font-bold"
+                color="success"
+                variant="shadow"
+              >
+                PAYMENT
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path="PrivacyPolicy" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-black font-bold" color="default" variant="shadow">PrivacyPolicy</Chip>
-        </main>
-      }>
-      </Route>
+        {/* <Route path="PrivacyPolicy" element={<PrivacyPolicy />}></Route> */}
 
-      <Route path="Filter" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="secondary" variant="shadow">FILTRO</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="Filter"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-white font-bold"
+                color="secondary"
+                variant="shadow"
+              >
+                FILTRO
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path="Details" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-black font-bold" color="warning" variant="shadow">DETALLES</Chip>
-        </main>
-      }>
-      </Route>
+        {/* <Route path="Details" element={<RestaurantDetail />}></Route> */}
 
-      <Route path="Routes" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="primary" variant="shadow">RUTAS</Chip>
-        </main>
-      }>
-      </Route>
+        <Route path="Routes" element={<RoutePlanner />}></Route>
 
-      <Route path="History" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="primary" variant="shadow">HISTORIAL</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="History"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-white font-bold"
+                color="primary"
+                variant="shadow"
+              >
+                HISTORIAL
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path="Settings" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-black font-bold" color="default" variant="shadow">Configuraciones</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="Settings"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-black font-bold"
+                color="default"
+                variant="shadow"
+              >
+                Configuraciones
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path="Analytics" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="danger" variant="shadow">Analiticas</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="Analytics"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-white font-bold"
+                color="danger"
+                variant="shadow"
+              >
+                Analiticas
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path="Menu" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="warning" variant="shadow">MENU</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="Menu"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-white font-bold"
+                color="warning"
+                variant="shadow"
+              >
+                MENU
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path="Login" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="primary" variant="shadow">Inicio de Sesion</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="Login"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-white font-bold"
+                color="primary"
+                variant="shadow"
+              >
+                Inicio de Sesion
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path="Register" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="secondary" variant="shadow">REGISTRO</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="Register"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-white font-bold"
+                color="secondary"
+                variant="shadow"
+              >
+                REGISTRO
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path="Reservations" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="secondary" variant="shadow">RESERVASCIONES</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="Reservations"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-white font-bold"
+                color="secondary"
+                variant="shadow"
+              >
+                RESERVASCIONES
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path="Reviews" element={
-        <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
-          <Chip className="text-white font-bold" color="danger" variant="shadow">RESEÑAS</Chip>
-        </main>
-      }>
-      </Route>
+        <Route
+          path="Reviews"
+          element={
+            <main className="w-full h-screen bg-gray-800 flex justify-center items-center">
+              <Chip
+                className="text-white font-bold"
+                color="danger"
+                variant="shadow"
+              >
+                RESEÑAS
+              </Chip>
+            </main>
+          }
+        ></Route>
 
-      <Route path='/*' element={< Navigate to="/Home" />} />
-    </Routes>
+        <Route path="/*" element={<Navigate to="/Home" />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { authSlice } from './auth.slice'
+import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./auth.slice";
+import routeReducer from "./routeSlice";
 
 
 export const store = configureStore({
   reducer: {
     authLogin: authSlice.reducer,
+    route: routeReducer, // Asegúrate de que el nombre aquí coincida con el utilizado en useSelector
+   
   },
-})
+});
