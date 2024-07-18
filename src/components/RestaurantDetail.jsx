4574fd359fa72d4ef825b47454b10a5b45fe253a
wulf <czh1983@gmail.com>
2024-07-18 09:45:46 -0300

    subiendo componente RestaurantDetail , con la Card correspondiente segun figma

---
+++ src/components/RestaurantDetail.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+
+export const RestaurantDetail = () => {
+  return (
+    <>
+
+<div className="max-w-2xl mx-auto">
+
+
+	<div className="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+		<a href="#">
+			<img className=" w-full rounded-t-lg p-8" src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80" alt="product image"/>
+        </a>
+			<div className="px-5 pb-5">
+				<a href="#">
+					<h3 className="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">About Restaurante
+						</h3>
+            <p className="text-gray-900  text-base tracking-tight dark:text-white">
+              Restaurante esta ubicado en medio de paisajes campestres, ofrece un ambiente acogedor y rústico . Sirve platos tradicionales , como asado , empanadas y locro.
+            </p>
+				</a>
+				<div className="flex items-center mt-2.5 mb-5">
+					<svg className="w-5 h-5 text-yellow-700" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-5 h-5 text-yellow-700" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-5 h-5 text-yellow-700" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-5 h-5 text-yellow-700" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-5 h-5 text-yellow-700" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<span className="  text-xs font-semibold mr-2 px-2.5 py-0.5 rounded ml-3">4.79 (78 reviews)</span>
+				</div>
+				<div className="flex items-center justify-between mt-2.5 mb-5">
+					<span className="text-xl font-bold text-gray-900 dark:text-white">Menú</span>
+				</div>
+        <div className="flex items-center justify-between mt-2.5   ">
+					<span className="text-xl font-bold text-gray-900 dark:text-white">Asado libre con bebida</span>
+
+				</div>
+          <p className="text-base  text-gray-700 dark:text-white"> Desde $10 por persona</p>
+        <div className="flex items-center justify-between mt-2.5  ">
+					<span className="text-xl font-bold text-gray-900 dark:text-white">Empanadas</span>
+				</div>
+        <p className="text-base  text-gray-700 dark:text-white mb-5"> Desde $10 la docena</p>
+					<a href="/Home"
+						className= "flex items-center flex-col text-white w-full bg-greenT  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Añadir a mi ruta
+						</a>
+			</div>
+	</div>
+</div>
+    
+    </>
+    
+  )
+}
