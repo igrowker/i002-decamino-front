8f3ed7cb888c96af604ea0f1d05e843d3fc42106
wulf <czh1983@gmail.com>
2024-07-23 20:47:50 -0300

    actualizacion de Card consumiendo la API del backend en algunas secciones

--- src/components/RestaurantDetail.jsx
+++ src/components/RestaurantDetail.jsx
@@ -1,13 +1,36 @@
-import React from "react";
 
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { Estrellas } from "./UI/Estrellas"
+import { useState , useEffect} from "react";
 
 export const RestaurantDetail = () => {
+  
+
+  const [restaurante , setRestaurante] = useState([])
+  useEffect(() => {
+   
+    const dataRestaurant = `https://decamino-back.onrender.com/api/restaurants`;
+      axios
+        .get(dataRestaurant)
+        .then((response) => {
+          const dataRes = response.data;
+          setRestaurante(dataRes);
+        })
+        .catch((error) => { 
+          console.error(error);
+        });
+  }, [setRestaurante])
+  
+  
+  
   return (
     <>
-      <div>
-        <div className="flex max-w-sm w-full bg-white shadow-md  mx-auto">
+    {restaurante.map((resto , id)=>{
+      return (
+
+      <div key={id}>
+        <div  className="flex max-w-sm w-full bg-white shadow-md  mx-auto">
           <div></div>
 
           <div
@@ -22,13 +45,16 @@ export const RestaurantDetail = () => {
               <div className="poster__info align-self-end w-full">
                 <div className="h-40"></div>
                 <h3 className="text-2xl font-semibold  text-white text-center pb-12">
-                  Restaurante
+                  {resto.title}
                 </h3>
                 <div className="space-y-6  bg-white rounded-t-xl p-5">
                   <div className="flex flex-col space-y-2 inner">
                     <div className="mb-0 text-lg text-gray-900 font-semibold ">
-                      About Restaurante
+                      About {resto.title}
                     </div>
+                      <p className="text-xs text-black mb-5">
+                        {resto.description}
+                      </p>
                   </div>
                   <div className="flex flex-row justify-between datos">
                     <div className="flex flex-col datos_col">
@@ -42,7 +68,9 @@ export const RestaurantDetail = () => {
                     </div>
                     <div className="flex flex-col datos_col">
                       <div className="release"></div>
-                      <div className="text-sm text-gray-400">Ver reviews</div>
+                      <div className="text-sm text-gray-400">
+                        <Link to="/">Ver reviews</Link>
+                        </div>
                     </div>
                   </div>
                   <div className="flex flex-col overview">
@@ -69,21 +97,24 @@ export const RestaurantDetail = () => {
             </div>
             <img
               className="absolute inset-0 transform w-full -translate-y-4"
-              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.geagp26pb91JxbnJ7ydeIQHaKu%26pid%3DApi&f=1&ipt=46bb4f9251a2789985307de852248fca556df7d6893393d37f8d96d1e702ba88&ipo=images"
+              src={resto.photos[0]}
             />
             <div className="bg-white flex flex-row relative pb-10 space-x-4 z-10 ">
-              <a
+              <Link
                 className="flex items-center py-2 px-28 rounded-lg mx-auto h-full text-white bg-greenT hover:bg-freshMint"
-                href="#"
+                to="#"
                 target="_blank"
                 data-unsp-sanitized="clean"
               >
                 <div className="text-sm text-white ml-2">Añadir a mi ruta</div>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
       </div>
+      )
+
+    })}
     </>
   );
 };
